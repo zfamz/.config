b4af3873a7feb646cee90ef4826be6762ed0c2f2
zfamz <812158392@qq.com>
2023-12-09 16:01:56 +0800

    feat(pnpm): add

--- config.zsh
+++ config.zsh
@@ -4,3 +4,6 @@ source ${MC_PATH}/zsh/aliases.zsh
 source ${MC_PATH}/zsh/env.zsh
 source ${MC_PATH}/zsh/vi.zsh
 source ${MC_PATH}/joshuto/config.zsh
+
+
+
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,1 +1,7 @@
 export EDITOR=nvim
+
+# pnpm
+export PNPM_HOME="$HOME/.local/share/pnpm"
+export PATH="$PNPM_HOME:$PATH"
+[[ -f ~/.config/tabtab/zsh/__tabtab.zsh ]] && . ~/.config/tabtab/zsh/__tabtab.zsh || true
+# pnpm end
